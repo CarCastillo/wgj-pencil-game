4d66ba001c2c96a59fa26ec3fe085c8884ec1a60
TheObands1 <juansebastian3080@hotmail.com>
2021-08-21 15:54:17 -0500

    Tweaked some changes in the map

--- PencilGame/Source/PencilGame/Private/WorldAssets/PG_Stapler.cpp
+++ PencilGame/Source/PencilGame/Private/WorldAssets/PG_Stapler.cpp
@@ -37,6 +37,7 @@ void APG_Stapler::BeginPlay()
 
 void APG_Stapler::LaunchPlayer(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult)
 {
+	
 	if (IsValid(OtherActor))
 	{
 			ACharacter* OverlappedCharacter = Cast<ACharacter>(OtherActor);
