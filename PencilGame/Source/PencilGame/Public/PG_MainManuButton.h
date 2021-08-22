ebdb97c23d7b60ce3356f40f0d61ca1ae3aa629c
CarCastillo <poleecastillo@gmail.com>
2021-08-22 11:21:37 -0500

    Added materials for assets and base for main menu widget

---
+++ PencilGame/Source/PencilGame/Public/PG_MainManuButton.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Blueprint/UserWidget.h"
+#include "PG_MainManuButton.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class PENCILGAME_API UPG_MainManuButton : public UUserWidget
+{
+	GENERATED_BODY()
+	
+};
