8b91875995b08aa6d7e2bf11832d24d4fa6d241c
CarCastillo <poleecastillo@gmail.com>
2021-08-22 13:38:27 -0500

    Added Main Menu widget

--- PencilGame/Source/PencilGame/Private/PG_MainMenuWidget.cpp
+++ PencilGame/Source/PencilGame/Private/PG_MainMenuWidget.cpp
@@ -2,4 +2,32 @@
 
 
 #include "PG_MainMenuWidget.h"
+#include "Kismet/GameplayStatics.h"
+#include "Kismet/KismetSystemLibrary.h"
+#include "GameFramework/PlayerController.h"
+#include "Sound/SoundCue.h"
 
+void UPG_MainMenuWidget::InitializeWidget()
+{
+	if (!IsValid(MainMenuMusic))
+	{
+		return;
+	}
+
+	UGameplayStatics::PlaySound2D(GetWorld(), MainMenuMusic);
+}
+
+void UPG_MainMenuWidget::NewGame()
+{
+	UGameplayStatics::OpenLevel(GetWorld(), GameplayLevelName);
+}
+
+void UPG_MainMenuWidget::GoToCredits()
+{
+}
+
+void UPG_MainMenuWidget::QuitGame()
+{
+	APlayerController* PlayerController = UGameplayStatics::GetPlayerController(GetWorld(), 0);
+	UKismetSystemLibrary::QuitGame(GetWorld(), PlayerController, EQuitPreference::Quit, false);
+}
--- PencilGame/Source/PencilGame/Public/PG_MainMenuWidget.h
+++ PencilGame/Source/PencilGame/Public/PG_MainMenuWidget.h
@@ -6,12 +6,34 @@
 #include "Blueprint/UserWidget.h"
 #include "PG_MainMenuWidget.generated.h"
 
-/**
- * 
- */
+class USoundCue;
+
 UCLASS()
 class PENCILGAME_API UPG_MainMenuWidget : public UUserWidget
 {
 	GENERATED_BODY()
 	
+protected:
+
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = "MainMenu")
+	FName GameplayLevelName;
+
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = "Audio")
+	USoundCue* MainMenuMusic;
+
+public:
+
+	UFUNCTION(BlueprintCallable)
+		void InitializeWidget();
+
+protected:
+
+	UFUNCTION(BlueprintCallable, Category = "MainMenu")
+	void NewGame();
+
+	UFUNCTION(BlueprintCallable, Category = "MainMenu")
+	void GoToCredits();
+
+	UFUNCTION(BlueprintCallable, Category = "MainMenu")
+	void QuitGame();
 };
